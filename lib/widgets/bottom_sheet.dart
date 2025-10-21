a08208abcf286bd986a55b8b75ead3eb55c0f162
hailey <ahhh0325@gmail.com>
2025-10-21 20:44:44 +0900

    refactor: BottomSheet 위젯 별도의 파일로 분리  (가독성)

---
+++ lib/widgets/bottom_sheet.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:tasks/widgets/buttons/description_button.dart';
+import 'package:tasks/widgets/buttons/favorite_button.dart';
+import 'package:tasks/widgets/buttons/save_button.dart';
+
+class BottomSheet extends StatefulWidget {
+  BottomSheet(this.empty, this.onEmptyChanged);
+
+  final bool empty;
+  final void Function(bool empty) onEmptyChanged;
+
+  @override
+  State<BottomSheet> createState() => _BottomSheetState();
+}
+
+class _BottomSheetState extends State<BottomSheet> {
+  bool spread = false;
+  void onSpreadChanged(bool newSpread) {
+    setState(() => spread = newSpread);
+  }
+
+  // TextField 입력값 받기
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+
+  bool filled = false;
+  @override
+  void initState() {
+    super.initState();
+    titleController.addListener(() {
+      setState(() {
+        filled = titleController.text.isNotEmpty;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StatefulBuilder(
+      builder: (BuildContext context, StateSetter setState) {
+        return SingleChildScrollView(
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+            top: 12,
+            left: 20,
+            right: 20,
+          ),
+          child: Column(
+            children: [
+              TextField(
+                controller: titleController,
+                autofocus: true,
+                onSubmitted: (value) {
+                  titleController.text.isEmpty
+                      ? snackBar(context)
+                      : saveToDo(
+                          context,
+                          widget.onEmptyChanged,
+                          titleController,
+                          descriptionController,
+                        );
+                },
+                decoration: InputDecoration(
+                  hintStyle: TextStyle(fontSize: 16),
+                  hintText: '새 할 일',
+                ),
+              ),
+              spread
+                  ? DescriptionField(
+                      context,
+                      widget.onEmptyChanged,
+                      titleController,
+                      descriptionController,
+                    )
+                  : SizedBox.shrink(),
+              Row(
+                children: [
+                  DescriptionButton(spread, onSpreadChanged),
+                  FavoriteButton(),
+                  Spacer(),
+                  SaveButton(
+                    widget.onEmptyChanged,
+                    filled,
+                    titleController,
+                    descriptionController,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+/*
+widgets
+-  description_button ) DescriptionButton, DescriptionField
+-  favorite_button ) FavoriteButton
+-  save_button ) SaveButton, saveToDo(), snackBar()
+*/
